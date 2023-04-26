4a57cd6682d34c80609df68a68ad7e7eb470fa23
marelh <e.marouane02@gmail.com>
2023-04-26 09:29:01 +0000

    "I just added a new command line runner"

---
+++ src/main/java/com/example/devops_tp1/DevopsTp1Application.java
@@ -0,0 +1,21 @@
+package com.example.devops_tp1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DevopsTp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsTp1Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner test(){
+        return (args -> {
+            System.out.println("My first modification");
+        });
+    }
+}
---
+++ src/test/java/com/example/devops_tp1/DevopsTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_tp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
